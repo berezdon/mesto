6e8f4a48962d6ed270145981f33a56786aa96c0f
Maksim Spirin <berezdon@yandex.ru>
2021-01-26 02:08:17 +0300

    feat: Написать js попап для отправки формы

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+let popUp = document.querySelector('.popup');
+let profileEditButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__save-button');
+
+profileEditButton.addEventListener('click', function () {
+  //popUp.style.display = 'flex'; - можно было и так, то в тз написано сделать через добавление и удаление стилей, эх...
+  popUp.classList.add('popup_opened');
+  let profileTitle = document.querySelector('.profile__title').textContent;
+  let profileSubtitle = document.querySelector('.profile__subtitle').textContent;
+  let nameInput = document.querySelector('.popup__firstname');
+  let jobInput = document.querySelector('.popup__profession');
+  nameInput.value = profileTitle;
+  jobInput.value = profileSubtitle;
+});
+
+closeButton.addEventListener('click', function () {
+  popUp.classList.remove('popup_opened');
+});
+
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  popUp.classList.remove('popup_opened');
+  let nameInput = document.querySelector('.popup__firstname').value;
+  let jobInput = document.querySelector('.popup__profession').value;
+  let profileTitle = document.querySelector('.profile__title');
+  let profileSubtitle = document.querySelector('.profile__subtitle');
+  profileTitle.textContent = nameInput;
+  profileSubtitle.textContent = jobInput;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('click', formSubmitHandler); 
